1a751056d5461fbd6d13dd2ba905ff5d58c47c19
digowfsa16 <rodrigo-fsa@live.com>
2024-12-06 14:23:56 -0300

    Create Main
    Primeiro projeto desafio

---
+++ main.js
@@ -0,0 +1,29 @@
+// 1️⃣ Desafio Classificador de nível de Herói
+
+//"""O Que deve ser utilizado**
+
+//- Variáveis
+
+let heroi = ['Goku',10001]
+let nivel = 'Radiante'
+
+let Niveis= [[0,1000,"Ferro"],
+[1001,2000,"Bronze"],
+[2001,5000,"Prata"],
+[5001,700,"Ouro"],
+[7001,8000,"Platina"],
+[8001,9000,"Ascendente"],
+[9001,10000,"Imortal"] ]
+
+
+//- Operadores
+//- Laços de repetição
+
+for (let i = 0; i < Niveis.length; i++) {
+    if (heroi[1] >= Niveis[i][0] && heroi[1] <= Niveis[i][1]) {
+        console.log(Niveis[i][0])
+        nivel = Niveis[i][2]
+    }
+}
+
+console.log(nivel)
